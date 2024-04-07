dedc4489c9ed8678671e9ba8e9f3d0b62de94e9c
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2024-04-07 08:25:44 +0300

    update

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 
-import vits_config,vits_model,vits_output,arguments,decoder,encoder,discriminator,duration_predictor,flow,posterior_encoder
-import PosteriorDecoderModel,plot,trainer
-import dataset_features_collector,feature_extraction
+# from . import vits_config,vits_model,vits_output,arguments,decoder,encoder,discriminator,duration_predictor,flow,posterior_encoder
+# from . import PosteriorDecoderModel,plot,trainer
+# from . import dataset_features_collector,feature_extraction
